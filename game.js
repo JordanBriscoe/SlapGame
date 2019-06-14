e2bb1e4505c1a0b642196568077fa8181f3520aa
JordanBriscoe <briscoe41@gmail.com>
2019-06-14 16:55:37 -0600

    First section done

--- game.js
+++ game.js
@@ -1,14 +1,18 @@
-let health = 100
-
-function slap() {
-    health --
-    
+let character = {
+    name: "enemy",
+    slap: 1,
+    punch: 5,
+    kick: 10,
+    health: 100
 }
+let attacks = 0;
 
-function punch() {
-    health -=5
-}
 
-function kick() {
-    health -=10
+function attack(type) {
+   character.health -= character[type]   
+   let healthElem = document.querySelector("#health")
+   healthElem.innerText = character.health.toString()
+   attacks ++
+   let attackElem = document.querySelector('#hitcounter')
+   attackElem.innerText = attacks.toString()
 }
