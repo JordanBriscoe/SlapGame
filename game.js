3e1c209eee7b42cc2970108e6fcd72729062473a
JordanBriscoe <briscoe41@gmail.com>
2019-06-17 11:42:26 -0600

    code almost done

--- game.js
+++ game.js
@@ -1,18 +1,61 @@
 let character = {
-    name: "enemy",
-    slap: 1,
-    punch: 5,
-    kick: 10,
-    health: 100
+   name: "enemy",
+   attacks: {
+      slap: 1,
+      punch: 5,
+      kick: 10
+   },
+   hits: 0,
+   health: 100,
+   items: []
 }
-let attacks = 0;
 
+
+let items = {
+   fire: {
+      name: 'FIRE',
+      modifier: 3,
+      description: 'BURNT'
+   },
+   ice: {
+         name: 'ICE',
+         modifier: 3,
+         description: 'FROZEN'
+      },
+      fatality: {
+         name: 'DEATH',
+         modifier: 20,
+         description: 'FATALITY'
+      }
+   }
+
+
+let healthElem = document.querySelector("#health")
+let attackElem = document.querySelector('#hitcounter')
 
 function attack(type) {
-   character.health -= character[type]   
-   let healthElem = document.querySelector("#health")
+   character.health -= character.attacks[type] + addMods() 
+   character.hits++
+   update()
+}
+function giveItem(item){
+   if()
+   character.items.push(items[item])
+}
+
+function update() {
    healthElem.innerText = character.health.toString()
-   attacks ++
-   let attackElem = document.querySelector('#hitcounter')
-   attackElem.innerText = attacks.toString()
+   attackElem.innerText = character.hits.toString()
+   
+}
+ update()
+
+
+function addMods() {
+   let modTotal = 0
+   for (let i=0; i < character.items.length; i++){
+      let item = character.items[i]
+      modTotal += item.modifier
+   }
+   return modTotal
 }
